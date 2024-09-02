5c4bdc16c957649e8d75c7667ead7cbd1863bc51
Imam Usmani <imam@e.email>
2024-09-02 21:36:31 +0200

    add tests

---
+++ tests/test_touchscreen.py
@@ -0,0 +1,27 @@
+import time
+import logging
+
+
+from waveshare_touch_epaper.touch_screen import GT1151
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def touch_screen():
+    print('normal test...')
+    gt = GT1151()
+    gt.start()
+    time.sleep(1)
+    gt.stop()
+
+    print('test with context manager')
+    with GT1151() as gt:
+        print('touch has started')
+        x, y, s = gt.input()
+    print(f'detected touch at {x}, {y},{s}')
+    print('touch should be closed')
+
+
+if __name__ == '__main__':
+    touch_screen()
