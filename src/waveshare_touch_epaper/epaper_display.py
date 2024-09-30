6d3da28c83b8838f806b16372b91a90b6d731401
Imam Usmani <imam@e.email>
2024-09-30 21:58:52 +0200

    meta epaper class

--- src/waveshare_touch_epaper/epaper_display.py
+++ src/waveshare_touch_epaper/epaper_display.py
@@ -1,2 +1,14 @@
 from abc import ABCMeta, abstractmethod
 import logging
+
+epaper_models = dict()
+
+
+class MetaEpaper(ABCMeta):
+
+    """meta class  for epaper displays to store class and their model in a dict"""
+
+    def __init__(cls, name, bases, dict):
+        """store the class and in a dict upon creation"""
+        ABCMeta.__init__(cls, name, bases, dict)
+        epaper_models[name] = cls
