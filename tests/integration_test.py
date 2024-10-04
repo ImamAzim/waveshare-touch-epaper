f0099bc5b49781e6c41d87c3ad787465353aa547
Imam Usmani <imam@e.email>
2024-10-04 21:30:47 +0200

    write integrated test

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,33 +1,36 @@
-import time
 from PIL import Image, ImageDraw
 
 
 from waveshare_touch_epaper.epaper_display import EPD2in13, EpaperException
-from waveshare_touch_epaper.touch_screen import GT1151, TouchEpaperException, BaseTouchScreen
+from waveshare_touch_epaper.touch_screen import GT1151, TouchEpaperException
 
 
 def touch_and_display_loop():
     try:
         width = EPD2in13.WIDTH
         height = EPD2in13.HEIGHT
-        img = Image.new('1', (width, height) 255)
+        img = Image.new('1', (width, height), 255)
         draw = ImageDraw.Draw(img)
         draw.text(width/2, height/2, 'touch me!')
         with GT1151() as gt, EPD2in13() as epd:
             epd.display(img, full=True)
             while True:
                 try:
-                    coordinates = gt.input(timeout=30)
+                    x, y, s = gt.input(timeout=30)
                 except TouchEpaperException:
                     print('no touch detected during timeout, exit')
                     break
                 else:
-                    con
-        pass
+                    length = s ** 0.5
+                    dx = length / 2
+                    draw.rectangle((x - dx, y - dx, x + dx, y + dx), fill=0)
+                    try:
+                        epd.display(img, full=False)
+                    except EpaperException:
+                        epd.display(img, full=True)
     except KeyboardInterrupt:
         print('goodbye')
 
 
 if __name__ == '__main__':
     touch_and_display_loop()
-
