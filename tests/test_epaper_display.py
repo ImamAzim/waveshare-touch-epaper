4f23bf4baef279a1039729c5a258226db13aa3c2
Imam Usmani <imam@e.email>
2024-10-06 22:32:53 +0200

    display test

--- tests/test_epaper_display.py
+++ tests/test_epaper_display.py
@@ -1,5 +1,8 @@
 import unittest
+import time
 
+
+from PIL import Image, ImageDraw
 
 from waveshare_touch_epaper import epaper_models
 
@@ -9,7 +12,15 @@ class TestEPD2in13Mock(unittest.TestCase):
     def test_mock_interface(self):
         epd = epaper_models['EPD2in13Mock']()
 
+def display():
+    with epaper_models['EPD2in13'] as epd:
+        img = Image.new('1', (epd.WIDTH, epd.HEIGHT), 255)
+        draw = ImageDraw.Draw(img)
+        x0 = epd.WIDTH // 2
+        y0 = epd.HEIGHT // 2
+        draw.text(x0, y0, 'hello world')
+        epd.display(img)
 
 
 if __name__ == '__main__':
-    pass
+    display()
